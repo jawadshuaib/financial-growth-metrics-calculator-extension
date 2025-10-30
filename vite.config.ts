99f34879d8d6cab5f327e4b576a788460446fc65
Jawad Shuaib <jawadshuaib@mac.lan>
2025-10-30 17:43:52 -0400

    Updated manifest with title and desc

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: resolve(__dirname, 'popup.html'),
-        background: resolve(__dirname, 'src/background/index.ts'),
         content: resolve(__dirname, 'src/content/index.ts'),
       },
       output: {
